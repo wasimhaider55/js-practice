438fe7a155036b3e35ebe9590074b64037e5b99f
wasim haider <wasimhaider14379@gmail.com>
2024-12-12 15:16:58 +0500

    data Operation done in javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,3 +37,48 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " wasim"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2"); Types of conversion
+
+// console.log(3 + 4 * 5 % 3); // donot massy code like this 
+
+
+// console.log(+true); // output 1
+// console.log(+"");  // output 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 // don't code like this 
+
+let gameCounter = 100
+++gameCounter;   // prefix increament
+// gameCounter++; // postfix increament
+console.log(gameCounter);
+
+// link to study prefix and postfix
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
