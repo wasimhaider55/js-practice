1327beb7e3250b49bfdfd905f8d87b5fe9861e35
wasim haider <wasimhaider14379@gmail.com>
2024-12-12 14:40:14 +0500

    conversion in data types in javascript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+let score = 33; 
+let name = "wasim haider";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof(name));
+
+let count = "44";
+
+let valueInNumber = Number(count);
+// console.log(typeof(valueInNumber));
+
+
+let counter = "444abc"
+
+let valueOfNumber = Number(counter);
+// console.log(typeof(valueOfNumber));
+// console.log(valueOfNumber); // NaN not a number 
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = "wasim";
+
+let BooleanIsLogin = Boolean(isLoggedIn);
+// console.log(BooleanIsLogin);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
