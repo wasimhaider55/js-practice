074caeb4aef8e31f37d369933218709717ccd145
wasim haider <wasimhaider14379@gmail.com>
2024-12-12 15:28:28 +0500

    comparision using data types in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===  triple equal check the " Data type also"
+
+// console.log("2" === 2);
