56cfcd169bc9dcb5d626728387863c29169a2521
wasim haider <wasimhaider14379@gmail.com>
2024-12-13 12:01:58 +0500

    string in javascript

---
+++ 01_basics/05_string.js
@@ -0,0 +1,42 @@
+const name = "wasim"
+const age = 25
+
+// console.log(name + repoCount + " Value"); 
+
+// console.log(`Hellow my name is ${name} and my age is ${age}`);
+
+const gameName = new String("freefire");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('f'));
+
+// How to find second 'F' in freefire
+// const firstIndex = gameName.indexOf('f');
+// const secondIndex = gameName.indexOf('f', firstIndex + 1);
+// console.log(secondIndex);
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   wasim    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://wasim.com/haider%20bangash"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('wasim'))
+
+const schoolName = new String("kahi-public-school")
+
+// console.log(schoolName.split('-')); //  convert into array with - 
