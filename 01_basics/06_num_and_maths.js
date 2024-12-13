b257f88fcfb8234ae810d7f2c18cf7afa1d4944a
wasim haider <wasimhaider14379@gmail.com>
2024-12-13 15:50:02 +0500

    num and math explain

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,42 @@
+const score = 49;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString()); // convert into string
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 13.44;
+// console.log(otherNumber.toPrecision(3));
+
+
+const number = 1000000;
+// console.log(number.toLocaleString());
+//formatting with English numerals in the Pakistan locale
+// console.log(number.toLocaleString('en-PK'));
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+// console.log((Math.round(Math.random() * 10) + 1 ));
+// console.log((Math.random() * 10) + 1 );
+
+
+const min = 10
+const max = 20
+
+console.log(Math.random());
+console.log(Math.random() * (max - min + 1 ));
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
