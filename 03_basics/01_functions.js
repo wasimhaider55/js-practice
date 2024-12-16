4e58a0531690175d03567f7cee2ce2ec3468f76f
wasim haider <wasimhaider14379@gmail.com>
2024-12-16 22:40:17 +0500

    know about pass obj and arrays in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -44,4 +44,43 @@ function loginUserMessage(username = "Haider"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("wasim"));
+// loginUserMessage("Haider") // just function called 
+// console.log(loginUserMessage("wasim"));
+
+
+// Rest operator
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000,5000))
+
+const user = {
+    username: "wasim",
+    age: 99
+}
+
+function handleObject(anyobject){
+    // console.log(`User name is ${anyobject.username} and age is ${anyobject.age}`);   
+}
+// handleObject(user)
+
+// pass the whole object here 
+handleObject({
+    username : "Haider",
+    age : 299
+})
+
+
+const myarr = [100,300,400,5000];
+
+function retrunSecondValue (anyarray) {
+return anyarray[2];
+}
+
+// console.log(retrunSecondValue(myarr));
+
+// pass the whole array here 
+// console.log(retrunSecondValue([100,300,400,5000]));
+
+
